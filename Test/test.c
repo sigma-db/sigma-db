b874f54030a5428a2b027eb6a454161078e13a90
David Luis Wiegandt <wigandtd@hu-berlin.de>
2021-09-17 17:15:56 +0200

    [WIP]

--- Test/test.c
+++ Test/test.c
@@ -12,67 +12,77 @@
 
 #include <stdnoreturn.h>
 
+#define suite_event(suite)                                                                         \
+    (union event_args)                                                                             \
+    {                                                                                              \
+        .suite = suite                                                                             \
+    }
+
+#define test_event(suite, name, fail_cnt)                                                          \
+    (union event_args)                                                                             \
+    {                                                                                              \
+        .test = { suite, name, fail_cnt }                                                          \
+    }
+
+#define assertion_event(test, lineno, message)                                                     \
+    (union event_args)                                                                             \
+    {                                                                                              \
+        .assertion = { test, lineno, message }                                                     \
+    }
+
+typedef int (*test_run_f)(union event_args *);
+typedef void (*report_f)(enum event_type, union event_args *);
+
 struct location {
     const char *file_name;
     const char *suite_name;
-    const char *test_name; // only required when inside a test function
-    int         lineno;    // only required for assertions/expectations
+    const char *test_name;
+    int         lineno;
 };
-
-typedef int (*test_run_f)(struct config);
-typedef void (*reporter_f)(struct location *sender, struct event_args);
 
 enum event_type {
     SUITE_BEGIN,
     SUITE_END,
     TEST_BEGIN,
-    TEST_FAILURE,
     TEST_END,
+    ASSERTION_FAILURE,
 };
 
-struct event_args {
-    enum event_type type;
-    union {
-        struct {
-            int test_cnt;
-            int fail_cnt;
-        } as_suite_end;
-
-        struct {
-            int         fail_cnt;
-            const char *message;
-        } as_test_failure;
-
-        struct {
-            const char *name;
-            int         fail_cnt;
-        } as_test_end;
-    };
+struct suite {
+    const char *name;
+    const char *file_name;
+    int         test_cnt;
+    int         fail_cnt;
+    jmp_buf *   buf;
 };
 
-struct context {
-    event_handler_f fail;
-    event_handler_f warn;
+struct test {
+    struct suite *suite;
+    const char *  name;
+    int           fail_cnt;
+};
+
+struct assertion {
+    struct test *test;
+    int          lineno;
+    const char * message;
+};
+
+union event_args {
+    struct suite     suite;
+    struct test      test;
+    struct assertion assertion;
 };
 
 #ifdef _WIN32
 static int enable_vt_mode();
 #endif // _WIN32
 
-static void console_reporter(struct event_args e, struct location *loc);
+static void          console_reporter(enum event_type, union event_args *);
+static noreturn void on_fail(union event_args *e, int lineno, const char *msg);
+static void          on_warn(union event_args *e, int lineno, const char *msg);
 
-static jmp_buf    error;
-static reporter_f report;
-
-static noreturn void failure_handler(struct location src, const char *desc)
-{
-    longjmp(error, 1);
-}
-
-static void warning_handler(struct location src, const char *msg)
-{
-    report((struct event_args){.type = TEST_FAILURE, .as_test_failure = {.message = msg}}, &src);
-}
+static report_f      report = console_reporter;
 
 int sigma_test_main(int argc, char *argv)
 {
@@ -81,16 +91,14 @@ int sigma_test_main(int argc, char *argv)
     SetConsoleOutputCP(CP_UTF8);
 #endif
     setlocale(LC_ALL, "en_US.UTF-8");
-
-    report = console_reporter;
 }
 
-int sigma_test_run_test(const char *name, test_f test, struct config ctx)
+int sigma_test_run_test(union event_args *e, const char *name, test_f test)
 {
-    struct location loc = {.test_name = name};
-    report((struct event_args){.type = TEST_BEGIN}, &loc);
-    int fail = !setjmp(error) ? test(ctx), 0 : 1;
-    report((struct event_args){.type = TEST_END, .as_test_end = {.fail_cnt = fail}}, &loc);
+    union event_args test_ctx = test_event(&e->suite, name, 0);
+    report(TEST_BEGIN, &test_ctx);
+    int fail = !setjmp(e->suite.buf) ? test(e, on_fail, on_warn), 0 : e->test.fail_cnt;
+    report(TEST_END, &test_ctx);
     return fail;
 }
 
@@ -98,57 +106,52 @@ int sigma_test_run_test_collection(const char *file_name, const char *suite_name
 {
     va_list    ap;
     test_run_f test;
-    // jmp_buf    error;
+    jmp_buf    error;
 
-    int test_cnt = 0;
-    int fail_cnt = 0;
-
-    struct config ctx = {
-        .fail = failure_handler,
-        .warn = warning_handler,
-        //.buf  = &error,
-    };
-    struct location loc = {
-        .file_name  = file_name,
-        .suite_name = suite_name,
+    struct suite suite = {
+        .name      = suite_name,
+        .file_name = file_name,
+        .test_cnt  = 0,
+        .fail_cnt  = 0,
+        .buf       = &error,
     };
 
-    report((struct event_args){.type = SUITE_BEGIN}, &loc);
+    report(SUITE_BEGIN, &suite_event(suite));
 
     va_start(ap, suite_name);
     while ((test = va_arg(ap, test_run_f)) != NULL) {
-        test_cnt += 1;
-        fail_cnt += test(ctx);
+        suite.test_cnt += 1;
+        suite.fail_cnt += test(&suite_event(suite));
     }
     va_end(ap);
 
-    report((struct event_args){.type         = SUITE_END,
-                               .as_suite_end = {.test_cnt = test_cnt, .fail_cnt = fail_cnt}},
-           &loc);
+    report(SUITE_END, &suite_event(suite));
 
-    return fail_cnt;
+    return suite.fail_cnt;
 }
 
-struct test {
-    const char *name;
-    int         test_cnt;
-    int         fail_cnt;
-};
-struct suite {
-    const char *name;
-};
-
-static void console_reporter(struct location *ctx, struct event_args e)
+static noreturn void on_fail(union event_args *e, int lineno, const char *msg)
 {
-    switch (e.type) {
+    report(ASSERTION_FAILURE, &assertion_event(&e->test, lineno, msg));
+    longjmp(e->test.suite->buf, 1);
+}
+
+static void on_warn(union event_args *e, int lineno, const char *msg)
+{
+    report(ASSERTION_FAILURE, &assertion_event(&e->test, lineno, msg));
+}
+
+static void console_reporter(enum event_type type, union event_args *e)
+{
+    switch (type) {
     case SUITE_BEGIN:
         write(bullet(PROMPT, CYAN),
-              format(cyan("%s ") dim("(%s)") "\n", ctx->suite_name, ctx->file_name));
+              format(cyan("%s ") dim("(%s)") "\n", e->suite.name, e->suite.file_name));
         break;
 
     case SUITE_END:
-        int test_cnt = e.as_suite_end.test_cnt;
-        int fail_cnt = e.as_suite_end.fail_cnt;
+        int test_cnt = e->suite.test_cnt;
+        int fail_cnt = e->suite.fail_cnt;
         fprintf(stdout, "\n");
         if (fail_cnt < test_cnt) {
             write(bullet(CHECKMARK, GREEN), success("%d passing\n", test_cnt - fail_cnt));
@@ -159,23 +162,24 @@ static void console_reporter(struct location *ctx, struct event_args e)
         break;
 
     case TEST_BEGIN:
-        write(indent(1), bullet(DOTTED_CIRCLE, WHITE), format("%s", ctx->test_name));
-        break;
-
-    case TEST_FAILURE:
-        // Before printing the *first* error message, we visually mark the test case as failing
-        if (e.as_test_failure.fail_cnt == 1) {
-            write(CR, indent(1), bullet(CROSSMARK, RED), error("%s\n", ctx->test_name));
-        }
-        write(indent(2),
-              bullet(BLACK_CIRCLE, YELLOW),
-              warn("Expectation on line %d failed: %s\n", ctx->lineno, e.as_test_failure.message));
+        write(indent(1), bullet(DOTTED_CIRCLE, WHITE), format("%s", e->test.name));
         break;
 
     case TEST_END:
-        if (e.as_test_end.fail_cnt == 0) {
-            write(CR, indent(1), bullet(CHECKMARK, GREEN), format("%s\n", ctx->test_name));
+        if (e->test.fail_cnt == 0) {
+            write(CR, indent(1), bullet(CHECKMARK, GREEN), format("%s\n", e->test.name));
         }
+        break;
+
+    case ASSERTION_FAILURE:
+        // Before printing the very first error message, we visually mark the test as failing
+        if (e->test.fail_cnt == 1) {
+            write(CR, indent(1), bullet(CROSSMARK, RED), error("%s\n", e->test.name));
+        }
+        write(
+            indent(2),
+            bullet(BLACK_CIRCLE, YELLOW),
+            warn("Expectation on line %d failed: %s\n", e->assertion.lineno, e->assertion.message));
         break;
     }
 }
--- Test/test.h
+++ Test/test.h
@@ -6,15 +6,15 @@
 
 #define TEST(name)                                                                                 \
     static void test_##name(struct config cfg);                                                    \
-    static int  name(struct config cfg)                                                            \
+    static int  name(union event_args *e)                                                          \
     {                                                                                              \
-        return sigma_test_run_test(#name, test_##name, cfg);                                       \
+        return sigma_test_run_test(e, #name, test_##name);                                         \
     }                                                                                              \
-    static void test_##name(struct config cfg)
+    static void test_##name(union event_args *e, event_handler_f fail, event_handler_f warn)
 
-#define FAIL(msg) cfg.fail(cfg, __LINE__, msg)
+#define FAIL(msg) fail(e, __LINE__, msg)
 
-#define WARN(msg) cfg.warn(cfg, __LINE__, msg)
+#define WARN(msg) warn(e, __LINE__, msg)
 
 #define ASSERT(cond)                                                                               \
     if (!(cond)) FAIL(#cond)
@@ -22,14 +22,11 @@
 #define EXPECT(cond)                                                                               \
     if (!(cond)) WARN(#cond)
 
-typedef void (*event_handler_f)(struct location, const char *);
+enum event_type;
+union event_args;
 
-struct config {
-    event_handler_f fail;
-    event_handler_f warn;
-};
-
-typedef void (*test_f)(struct config);
+typedef void (*event_handler_f)(union event_args *, int, const char *);
+typedef void (*test_f)(union event_args *, event_handler_f, event_handler_f);
 
 /**
  * Process forwarded command line arguments to configure the test engine
@@ -39,7 +36,7 @@ int sigma_test_main(int argc, char *argv);
 /**
  * Runs a test and returns 0 on success
  */
-int sigma_test_run_test(const char *name, test_f test, struct config ctx);
+int sigma_test_run_test(union event_args *e, const char *name, test_f test);
 
 /**
  * Runs a collection of tests and returns the number of failed tests
